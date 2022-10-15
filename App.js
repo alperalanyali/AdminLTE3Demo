7dd7f11b004e2f9e6d0e714dc77d1c42dc1ca488
Alper alanyalie <alanyalialper@gmail.comm>
2022-10-15 18:46:48 +0300

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import * as shape from 'd3-shape'
+
+import { AreaChart, Grid } from 'react-native-svg-charts'
 import { StyleSheet, Text, View } from 'react-native';
 
+import React from 'react';
+import { StatusBar } from 'expo-status-bar';
+
 export default function App() {
+  const data = [50, 10, 40, 95, -4, -24, 85, 91, 35, 53, -53, 24, 50, -20, -80]
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.js to dsfsdf working on your app!</Text>
       <StatusBar style="auto" />
+      <AreaChart
+                style={{ height: 200 }}
+                data={data}
+                contentInset={{ top: 30, bottom: 30 }}
+                curve={shape.curveNatural}
+                svg={{ fill: 'rgba(134, 65, 244, 0.8)' }}
+            >
+                <Grid />
+            </AreaChart>
     </View>
   );
 }
